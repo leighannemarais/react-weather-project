e993a6791bcd41d1005c6f5d1581bcf237005ac1
leighannemarais <leighannemarais@gmail.com>
2023-08-01 10:41:23 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Leigh-Anne Marais and is{" "}
-        <a
-          href="https://github.com/leighannemarais/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Leigh-Anne Marais and is{" "}
+          <a
+            href="https://github.com/leighannemarais/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
