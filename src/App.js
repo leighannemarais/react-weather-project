d0179ffa605fe48a30a2b58dd2cedd083c22cc07
leighannemarais <leighannemarais@gmail.com>
2023-08-01 10:25:58 +0200

    Added github repo link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Leigh-Anne Marais and is{" "}
+        <a
+          href="https://github.com/leighannemarais/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
